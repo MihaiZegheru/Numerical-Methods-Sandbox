4fe8d5801eebbcf929866d6203a5c58b490c9100
MihaiZegheru <mihai.z.2005@gmail.com>
2024-04-28 13:54:25 +0300

    Updated SVD
    Signed-off-by: MihaiZegheru <mihai.z.2005@gmail.com>

--- SVD.py
+++ SVD.py
@@ -2,61 +2,32 @@ import numpy as np
 from eigen import eigen
 
 def SVD_right_ext(A: np.matrix, n: int, m: int):
-
 	eigen_values, eigen_vectors = eigen(np.transpose(A) * A)
-
-	eigen_values = np.transpose(np.matrix(eigen_values))
-	singular_values = np.sqrt(eigen_values[0:n])
-
-
-	S = np.zeros((n, n))
-
-	np.fill_diagonal(S, singular_values)
-
+	S = np.sqrt(eigen_values[0:n])
 	Vh = np.concatenate(eigen_vectors[0:n], axis=1)
-
 
 	U = A * Vh
 	for i in range(0, n):
-		U[0:m, i] /= singular_values[i]
-
-
+		U[0:m, i] /= S[i]
 	Vh = np.transpose(np.concatenate(eigen_vectors[0:n], axis=1))
 
-	return 0
+	return (U, S, Vh)
 
 def SVD_left_ext(A: np.matrix, n: int, m: int):
-
 	eigen_values, eigen_vectors = eigen(np.transpose(A) * A)
-
-	eigen_values = np.transpose(np.matrix(eigen_values))
-	singular_values = np.sqrt(eigen_values[0:m])
-	print(singular_values)
-
-	S = np.zeros((m, m))
-
-	np.fill_diagonal(S, singular_values)
-
+	S = np.sqrt(eigen_values[0:m])
 	Vh = np.concatenate(eigen_vectors[0:m], axis=1)
-	print(Vh)
 
 	U = A * Vh
 	for i in range(0, m):
-		U[0:n, i] /= singular_values[i]
-	print(U)
-	print()
-
+		U[0:n, i] /= S[i]
 	Vh = np.transpose(np.concatenate(eigen_vectors[0:m], axis=1))
 
-	return 0
+	return (U, S, Vh)
 
-def SVD():
+def SVD(A: np.matrix, r: int):
 	(n, m) = np.shape(A)
-
 	if n < m:
 		return SVD_right_ext(A, n, m)
 	else:
 		return SVD_left_ext(A, n, m)
-
-A = np.matrix('1 2 4; 2 3 1')
-SVD()
